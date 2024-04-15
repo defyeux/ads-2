439bca8c7997750533b42951a19cb03c0ff01839
unknown <230487@astanait.edu.kz>
2024-04-15 11:12:29 +0500

    yeux

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int lst[] = new int[] {1, 2, 3, 4, 5};
+        System.out.println(first(lst, 4));
+    }
+
+    private static int first(int[] lst, int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (lst[n - 1] % 2 == 0) {
+            return first(lst, n - 1);
+        }
+
+        return 1 + first(lst, n - 1);
     }
 }
